80e954992195f0257ecc46f71ca0d42c01798a1f
yangguo <yangguo@noxgroup.com>
2026-02-04 14:39:14 +0800

    update package version.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use tracing::info;
-use tracing_appender::non_blocking::NonBlocking;
-use tracing_subscriber::fmt;
-use tracing_subscriber::fmt::format::FmtSpan;
-use tracing_subscriber::fmt::writer::MakeWriterExt;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use axum_scaffold::configuration::{get_active_settings, get_configuration, get_log_file_appender, get_log_level, init_mysql_pool, init_tracing, run};
+use axum_scaffold::configuration::{get_configuration, init_mysql_pool, init_tracing, run};
 
 #[tokio::main]
 async fn main() {
